92a2585ec7262516968619a16bef6704b1098582
chataeg_MacBook <chataeg@gmail.com>
2023-05-16 14:47:06 +0900

    docs: add const

--- logic.js
+++ logic.js
@@ -13,8 +13,11 @@ var config = {
 var game = new Phaser.Game(config);
 var objs = [];
 
-
-
+// 상수 선언부
+const OBJS_START_POS_X = 400;
+const OBJS_START_POS_Y = 500;
+const OBJS_SPACE = 30;
+const REAR_IND = 19;
 
 
 // Define game variables
@@ -22,7 +25,7 @@ var characters = ['A', 'B', 'C']; // Array of character types
 var characterIndex = 0; // Current character index
 var score = 0; // Player's score
 var time = 30; // Initial time limit in seconds
-var rearind = 19;
+
 let debounceTimer;
 // Preload assets
 function preload() {
@@ -55,15 +58,13 @@ function update() {
   
   // characters[Math.floor(Math.random() * characters.length)]
   
-  for(let i = rearind ; i > -1 ; i--)
+  for(let i = REAR_IND ; i > -1 ; i--)
   {
-      this.add.sprite(400, 500 - i * 30, objs[i]);
+      this.add.sprite(OBJS_START_POS_X, OBJS_START_POS_Y - i * OBJS_SPACE, objs[i]);
   }
 
-//수정해야 할 사항
-// 키보드 떼기 전까지 갱신 X 
-// 키보드 입력과 이미지 파일이 맞는지 확인 
-  
+
+// 키보드 입력 1회 인식 및 갱신
 if (Phaser.Input.Keyboard.JustDown(KeyInput.left)) {
   if (isA()) invalidate();
 }
@@ -81,7 +82,7 @@ else if (Phaser.Input.Keyboard.JustDown(KeyInput.right)) {
 }
 
 function initObjs() {
-  for(let i = 0 ; i <= rearind ; i++)
+  for(let i = 0 ; i <= REAR_IND ; i++)
   {
       objs[i] = characters[Math.floor(Math.random() * characters.length)];
   }
@@ -92,20 +93,14 @@ function printObjs()
       document.write(objs+ "<br>");
 }
 
-// function addObjs()
-// {
-  
-//   objs[rearind] = characters[Math.floor(Math.random() * characters.length)];
-//   rearind++;
-// }
 
 function invalidate()
 {
-  for(let i = 0 ; i < rearind ; i++)
+  for(let i = 0 ; i < REAR_IND ; i++)
   {
       objs[i] = objs[i+1];
   }
-  objs[rearind] = characters[Math.floor(Math.random() * characters.length)];
+  objs[REAR_IND] = characters[Math.floor(Math.random() * characters.length)];
 }
 
 
